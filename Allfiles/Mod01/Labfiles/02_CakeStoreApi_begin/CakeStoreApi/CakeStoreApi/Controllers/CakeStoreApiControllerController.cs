b39347c560c2a1df37bc41a4b49d4e0786ecb5b8
Olaf van der Kruk <OlafCL@hotmail.com>
2019-11-18 10:51:53 +0100

    CakeStoreApi completed

---
+++ Allfiles/Mod01/Labfiles/02_CakeStoreApi_begin/CakeStoreApi/CakeStoreApi/Controllers/CakeStoreApiControllerController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CakeStoreApi.Models;
+
+namespace CakeStoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CakeStoreApiControllerController : ControllerBase
+    {
+        private IData _data;
+        public CakeStoreApiControllerController(IData data)
+        {
+            _data = data;
+        }
+        [HttpGet("/api/CakeStore")]
+        public ActionResult<List<CakeStore>> GetAll()
+        {
+            return _data.CakesInitializeData();
+        }
+        [HttpGet("/api/CakeStore/{id}")]
+        public ActionResult<CakeStore> GetById(int? id)
+        {
+            var item = _data.GetCakeById(id);
+            if(item == null)
+            {
+                return new NotFoundResult();
+            }
+            return new ObjectResult(item);
+        }
+    }
+}
