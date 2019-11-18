1af765b3c7a3ccdb1dd0e13329d336c5b498cee5
Olaf van der Kruk <OlafCL@hotmail.com>
2019-11-18 10:51:34 +0100

    ActorsRazorPages sample completed

---
+++ Allfiles/Mod01/Labfiles/01_ActorsRazorPages_begin/ActorsRazorPages/ActorsRazorPages/Pages/Actors/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ActorsRazorPages.Models;
+
+namespace ActorsRazorPages.Pages.Actors
+{
+    public class IndexModel : PageModel
+    {
+        private IData _data;
+        public IList<Actor> Actors { get; set; }
+        public IndexModel(IData data)
+        {
+            _data = data;
+        }
+        public void OnGet()
+        {
+            Actors = _data.ActorsInitializeData();
+        }
+    }
+}
