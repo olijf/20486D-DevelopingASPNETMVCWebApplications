168310989aaf8eb57340a1d3e87b668b94db4247
Olaf van der Kruk <OlafCL@hotmail.com>
2019-11-19 15:59:27 +0100

    Butterflies lab done

--- Allfiles/Mod06/Labfiles/01_ButterfliesShop_begin/ButterfliesShop/Controllers/ButterflyController.cs
+++ Allfiles/Mod06/Labfiles/01_ButterfliesShop_begin/ButterfliesShop/Controllers/ButterflyController.cs
@@ -6,6 +6,7 @@ using ButterfliesShop.Models;
 using ButterfliesShop.Services;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using System.IO;
 
 namespace ButterfliesShop.Controllers
 {
@@ -21,6 +22,12 @@ namespace ButterfliesShop.Controllers
             _environment = environment;
             _butterfliesQuantityService = butterfliesQuantityService;
             InitializeButterfliesData();
+        }
+        public IActionResult Index()
+        {
+            var indexViewModel = new IndexViewModel();
+            indexViewModel.Butterflies = _data.ButterfliesList;
+            return View(indexViewModel);
         }
 
         private void InitializeButterfliesData()
@@ -40,11 +47,65 @@ namespace ButterfliesShop.Controllers
             Butterfly requestedButterfly = _data.GetButterflyById(id);
             if (requestedButterfly != null)
             {
-                return null;
+                var webRootPaht = _environment.WebRootPath;
+                var folderPath = @"images\";
+                var fullPath = Path.Combine(webRootPaht, folderPath, requestedButterfly.ImageName);
+                if (System.IO.File.Exists(fullPath))
+                {
+                    var fileOnDisk = new FileStream(fullPath, FileMode.Open);
+                    byte[] fileBytes;
+                    using (var br = new BinaryReader(fileOnDisk))
+                    {
+                        fileBytes = br.ReadBytes((int)fileOnDisk.Length);
+
+                    }
+                    return File(fileBytes, requestedButterfly.ImageMimeType);
+                }
+                else
+                {
+                    if (requestedButterfly.PhotoFile.Length > 0)
+                    {
+                        return File(requestedButterfly.PhotoFile, requestedButterfly.ImageMimeType);
+                    }
+                }
+                return NotFound();
             }
             else
             {
                 return NotFound();
+            }
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Butterfly butterfly)
+        {
+            if (ModelState.IsValid)
+            {
+                var lastButterfly = _data.ButterfliesList.LastOrDefault();
+                butterfly.CreatedDate = DateTime.Today;
+                if (butterfly.PhotoAvatar != null && butterfly.PhotoAvatar.Length > 0)
+                {
+                    butterfly.ImageMimeType = butterfly.PhotoAvatar.ContentType;
+                    butterfly.ImageName = Path.GetFileName(butterfly.PhotoAvatar.FileName);
+                    butterfly.Id = lastButterfly.Id + 1;
+                    _butterfliesQuantityService.AddButterfliesQuantityData(butterfly);
+                    using (var ms = new MemoryStream())
+                    {
+                        butterfly.PhotoAvatar.CopyTo(ms);
+                        butterfly.PhotoFile = ms.ToArray();
+                    }
+                    _data.AddButterfly(butterfly);
+                    return RedirectToAction(nameof(Index));
+                }
+                return View(butterfly);
+            }
+            else
+            {
+                return View(butterfly);
             }
         }
     }
--- Allfiles/Mod06/Labfiles/01_ButterfliesShop_begin/ButterfliesShop/Models/Butterfly.cs
+++ Allfiles/Mod06/Labfiles/01_ButterfliesShop_begin/ButterfliesShop/Models/Butterfly.cs
@@ -2,10 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
 
 namespace ButterfliesShop.Models
 {
     public class Butterfly
     {
+        public int Id { get; set; }
+        [Display(Name = "Common Name")]
+        [Required(ErrorMessage = "Please enter Butterfly name")]
+        public string CommonName { get; set; }
+        [Display(Name = "Butterfly Family:")]
+        [Required(ErrorMessage = "Please enter Butterfly family")]
+        public Family? ButterflyFamily { get; set; }
+        [Display(Name = "Butterflies Quantity:")]
+        [Required(ErrorMessage = "Please enter Butterfly quantity")]
+        public int? Quantity { get; set; }
+        [Display(Name = "Characteristics:")]
+        [Required(ErrorMessage = "Please enter Butterfly characteristics")]
+        [StringLength(maximumLength: 50)]
+        public string Characteristics { get; set; }
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Updated on:")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yy}")]
+        public DateTime CreatedDate { get; set; }
+        [Display(Name = "Butterflies Picture:")]
+        [Required(ErrorMessage ="Please select the butterflies picture")]
+        public IFormFile PhotoAvatar { get; set; }
+        public string ImageName { get; set; }
+        public byte[] PhotoFile { get; set; }
+        public string ImageMimeType { get; set; }
     }
 }
