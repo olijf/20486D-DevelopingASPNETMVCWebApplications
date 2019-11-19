c2c6892c44e17ae2a63f94948e2a2c832a729e6a
Olaf van der Kruk <OlafCL@hotmail.com>
2019-11-19 11:38:48 +0100

    WorldJourney Done

---
+++ Allfiles/Mod04/Labfiles/01_WorldJourney_begin/WorldJourney/Controllers/CityController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using WorldJourney.Models;
+using WorldJourney.Filters;
+
+namespace WorldJourney.Controllers
+{
+    public class CityController : Controller
+    {
+        private IData _data;
+        private IHostingEnvironment _environment;
+        public CityController(IData data, IHostingEnvironment hosting)
+        {
+            _data = data;
+            _environment = hosting;
+            _data.CityInitializeData();
+        }
+        [Route("WorldJourney")]
+        [ServiceFilter(typeof(LogActionFilterAttribute))]
+        public IActionResult Index()
+        {
+            ViewData["Page"] = "Search City";
+            return View();
+        }
+        [Route("CityDetails/{id?}")]
+        public IActionResult Details(int? Id)
+        {
+            ViewData["Page"] = "Selected City";
+            City city = _data.GetCityById(Id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.Title = city.CityName;
+            return View(city);
+        }
+        public IActionResult GetImage(int? CityId)
+        {
+            ViewData["Message"] = "Display Image";
+            City requestedCity = _data.GetCityById(CityId);
+            if (requestedCity != null)
+            {
+                var webRootPath = _environment.WebRootPath;
+                var folderPath = @"images\";
+                var fullPath = Path.Combine(webRootPath, folderPath, requestedCity.ImageName);
+                var fileOnDisk = new FileStream(fullPath, FileMode.Open);
+                Byte[] fileBytes;
+                using (var br = new BinaryReader(fileOnDisk))
+                {
+                    fileBytes = br.ReadBytes((int)fileOnDisk.Length);
+                }
+                return File(fileBytes, requestedCity.ImageMimeType, fileOnDisk.Name);
+
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ Allfiles/Mod04/Labfiles/01_WorldJourney_begin/WorldJourney/Filters/LogActionFilterAttribute.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WorldJourney.Filters
+{
+    public class LogActionFilterAttribute : ActionFilterAttribute
+    {
+        private IHostingEnvironment _environment;
+        private string _contentRootPath;
+        private string _logPath;
+        private string _fileName;
+        private string _fullPath;
+        public LogActionFilterAttribute(IHostingEnvironment environment)
+        {
+            _environment = environment;
+            _contentRootPath = _environment.ContentRootPath;
+            _logPath = Path.Combine(_contentRootPath, @"LogFile\");
+            _fileName = $"log {DateTime.Now.ToString("MM-dd-yyyy-H-mm")}.txt";
+            _fullPath = Path.Combine(_logPath, _fileName);
+        }
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            Directory.CreateDirectory(_logPath);
+            var actionName = filterContext.ActionDescriptor.RouteValues["action"];
+            var controllerName = filterContext.ActionDescriptor.RouteValues["controller"];
+            using (var fs = new FileStream(_fullPath, FileMode.Create))
+            {
+                using (var sw = new StreamWriter(fs))
+                {
+                    sw.WriteLine($"The action {actionName} in {controllerName} controller started, event fired: OnActionExecuting");
+                }
+            }
+            base.OnActionExecuting(filterContext);
+        }
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            var actionName = context.ActionDescriptor.RouteValues["action"];
+            var controllerName = context.ActionDescriptor.RouteValues["controller"];
+            using (var fs = new FileStream(_fullPath, FileMode.Create))
+            {
+                using (var sw = new StreamWriter(fs))
+                {
+                    sw.WriteLine($"The action {actionName} in {controllerName} controller finished, event fired: OnActionExecuted");
+                }
+            }
+            base.OnActionExecuted(context);
+        }
+        public override void OnResultExecuted(ResultExecutedContext context)
+        {
+            var actionName = context.ActionDescriptor.RouteValues["action"];
+            var controllerName = context.ActionDescriptor.RouteValues["controller"];
+            var result = (ViewResult)context.Result;
+            using (var fs = new FileStream(_fullPath, FileMode.Create))
+            {
+                using (var sw = new StreamWriter(fs))
+                {
+                    sw.WriteLine($"The action {actionName} in {controllerName} controller has the following viewData : {result.ViewData.Values.FirstOrDefault()}, event fired: OnResultExecuted");
+                }
+            }
+            base.OnResultExecuted(context);
+        }
+    }
+}
